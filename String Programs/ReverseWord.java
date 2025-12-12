9d2a6bc35ef8c4dfa593578918d61e319203d574
Prakruthi G H <147101194+Prakruthi-G-H@users.noreply.github.com>
2025-12-12 15:23:53 +0530

    Update ReverseWord.java

--- String Programs/ReverseWord.java
+++ String Programs/ReverseWord.java
@@ -2,19 +2,6 @@ package programming_string;
 
 import java.util.Scanner;
 
-//public class ReverseWord {
-//
-//	public static void main(String[] args) {
-//	System.out.println("Let's begin new chapter from here ");
-//	String sen="Java";
-//	for (int i =sen.length()-1; i >=0 ; i--) {
-//		System.out.print(sen.charAt(i));
-//	}
-//	}
-//
-//}
-
-
 public class ReverseWord {
 
 	public static void main(String[] args) {
@@ -28,3 +15,4 @@ public class ReverseWord {
 	}
 
 }
+
