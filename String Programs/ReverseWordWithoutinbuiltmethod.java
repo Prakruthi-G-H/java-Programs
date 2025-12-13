58cf02f61205ba56ab7fd4c31de677e6e9577649
Prakruthi G H <147101194+Prakruthi-G-H@users.noreply.github.com>
2025-12-13 21:26:59 +0530

    Add files via upload

---
+++ String Programs/ReverseWordWithoutinbuiltmethod.java
@@ -0,0 +1,17 @@
+package programming_string;
+
+import java.util.Scanner;
+
+public class ReverseWordWithoutinbuiltmethod {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		char[] arr=s.toCharArray();
+		for (int i = arr.length-1; i >=0 ; i--) {
+			System.out.print(arr[i]);
+		}
+		
+	}
+
+}
