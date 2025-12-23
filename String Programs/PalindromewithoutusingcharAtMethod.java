e59b39ec5a4c05803751b4a19929b324d4714db5
Prakruthi G H <147101194+Prakruthi-G-H@users.noreply.github.com>
2025-12-23 12:53:25 +0530

    Add files via upload

---
+++ String Programs/PalindromewithoutusingcharAtMethod.java
@@ -0,0 +1,23 @@
+package programming_string;
+
+import java.util.Scanner;
+
+public class PalindromewithoutusingcharAtMethod {
+
+	public static void main(String[] args) {
+//      String s="java";
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+      char[] arr=s.toCharArray();
+      String rev="";
+      for (int i = arr.length-1; i >=0 ; i--) {
+		rev=rev+arr[i];
+	}
+      if (rev.equals(s)) {
+		System.out.println("Palindrome");
+	} else {
+  System.out.println("Not a Palindrome");
+	}
+	}
+
+}
